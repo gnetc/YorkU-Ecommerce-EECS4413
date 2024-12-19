8acf4a4c05db55b17c233c7cf52696bd3d0da428
gnetc <gnet@my.yorku.ca>
2024-12-18 22:34:59 -0500

    product updates

--- src/main/java/com/yorku/ecommerce/controller/ProductController.java
+++ src/main/java/com/yorku/ecommerce/controller/ProductController.java
@@ -7,12 +7,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.yorku.ecommerce.DAO.ProductDAO;
 import com.yorku.ecommerce.model.Product;
-
+import com.yorku.ecommerce.service.ProductService;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -26,39 +27,35 @@ public class ProductController {
     @Autowired
     private ProductDAO productDAO;
 
+    @Autowired
+    private ProductService productService;
+
     @GetMapping
-    public List<Product> getAllProducts(
-    @RequestParam(required = false) Integer categoryId,
-    @RequestParam(required = false) Integer brandId,
-    @RequestParam(required = false) String search,
-    @RequestParam(required = false) String sort) {
+    public ResponseEntity<List<Product>> getAllProducts(
+            @RequestParam(required = false) Integer categoryId,
+            @RequestParam(required = false) Integer brandId,
+            @RequestParam(required = false) String search,
+            @RequestParam(required = false) String sort) {
    
-        return productDAO.findAll(categoryId, brandId, search, sort);
+            List<Product> products = productService.getAllProducts(categoryId, brandId, search, sort);
+            return ResponseEntity.ok(products);
     }
 
     @PostMapping
     public void createProduct(@RequestBody Product product) {
         productDAO.save(product);
     }
-
-    @GetMapping("/products")
-    public String getMethodName(@RequestParam String param) {
-        return new String();
-    }
-
-    
    
     @GetMapping("/{id}")
    public ResponseEntity<Product> getProductById(@PathVariable Integer id) {
-    Product product = productDAO.findById(id);
+        Product product = productDAO.findById(id);
 
-    if (product != null) {
-        return ResponseEntity.ok(product);
-    } else {
-        return ResponseEntity.notFound().build();
+        if (product != null) {
+            return ResponseEntity.ok(product);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
-
-   }
 
     
  }
